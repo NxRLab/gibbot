5d50a677887c359404f6d02b61c6e6161672c9ff
Neal Ehardt <nealehardt@gmail.com>
2013-04-16 17:04:23 -0500

    commenting matlab/mb

--- sims/matlab/mb/mbmdl.m
+++ sims/matlab/mb/mbmdl.m
@@ -14,7 +14,7 @@ g = 9.81; % gravity
 % dimensionalized constants
 pd = [g I I L L m m r1 r2];
 
-X0 = zeros(numel(x0)*2, 1);
+X0 = zeros(numel(x0)*2, 1); % Allows for many initial conditions to be input
 X0(3:8:end) = x0(1:4:end);
 X0(4:8:end) = x0(2:4:end);
 X0(7:8:end) = x0(3:4:end);
--- sims/matlab/mb/mbode.m
+++ sims/matlab/mb/mbode.m
@@ -6,22 +6,26 @@ else
     odeoptions = odeset('RelTol',1e-6,'AbsTol',1e-6);
 end
 
-if isfield(S, 't0')
+if isfield(S, 't0')    % For multiple runs time updates correctly
     t = S.t0;
 else
     t = 0;
 end
 
 k = 1;
-p = S.p;
-X = S.X0;
-slns(N) = ode45(@(t, x) 0*x, 0:1, X, odeoptions); % get fields in struct
-F = @(t, X) mbeom(t, X, @(t, X) U(t, X, S), p);
-% F = @(t, X) S.T*mbeom(t, X, @(t, X) U(t, X, S), p);
+p = S.p;            % Get parameters
+X = S.X0;           % Get initial conditions
+slns(N) = ode45(@(t, x) 0*x, 0:1, X, odeoptions); % get fields in struct (Array initializations for number of impacts)
+
+% if ode45 detects state for balancing controller then update F for
+% balancing controller until ode45 recognizes balanced state
+
+F = @(t, X) mbeom(t, X, @(t, X) U(t, X, S), p); 
+% F = @(t, X) S.T*mbeom(t, X, @(t, X) U(t, X, S), p); % If you want to normalize time between 0 and 1
 
 for n = 1:N
     % tspan = [t, t + 1 + 0*S.T(k)];
-    tspan = [t, t + S.T(k)];
+    tspan = [t, t + S.T(k)];       %Tell time you want it to run for (Time interval for integration)
     
     % integrate
     slns(n) = ode45(F, tspan, X, odeoptions);
@@ -29,10 +33,10 @@ for n = 1:N
     % impact
     X = mbimp(slns(n).y(:,end), p);
     
-    % update
+    % updates x-y coordinates (Don't need for swing up)
     t = slns(n).x(end);
     
-    k = k + 1;
+    k = k + 1;                  %Used if you enter T as an array
     if k > numel(S.T)
         k = 1;
     end
