477968900d19e6b5d855fe6362ce317995896534
Gibbot Vision <admin@northwestern.edu>
2013-05-24 18:46:48 -0500

    Cleaned up Code
    Deleted unnecessary code and moved code from main into separate
    functions

--- Vision/calib/calib/calib.cpp
+++ Vision/calib/calib/calib.cpp
@@ -22,10 +22,270 @@ using namespace cv;
 using namespace std;
 
 // Constants
-#define RECORDED_FRAMES 3
+#define RECORDED_FRAMES 15
 #define NUMBER_OF_POINTS 13
 #define X_FRAMES 100
 
+
+ //== Declarations of functions ==--
+vector<vector<Point2f>> GetImagePoints(cModuleSync * sync);
+vector<vector<Point3f>> MakeObjectPoints();
+void printMat(const Mat& m);
+static Mat prepareCameraMatrix(Mat& cameraMatrix0, int rtype);
+static Mat prepareDistCoeffs(Mat& distCoeffs0, int rtype);
+static void collectCalibrationData( InputArrayOfArrays objectPoints,
+                                    InputArrayOfArrays imagePoints1,
+                                    InputArrayOfArrays imagePoints2,
+                                    Mat& objPtMat, Mat& imgPtMat1, Mat* imgPtMat2,
+                                    Mat& npoints );
+double mycalibrateCamera( InputArrayOfArrays _objectPoints,
+                            InputArrayOfArrays _imagePoints,
+                            Size imageSize, InputOutputArray _cameraMatrix, InputOutputArray _distCoeffs,
+                            OutputArrayOfArrays _rvecs, OutputArrayOfArrays _tvecs, int flags, TermCriteria criteria );
+bool runCalibration( Size& imageSize, Mat& cameraMatrix, Mat& distCoeffs, vector<vector<Point3f> >& objectPoints, 
+                    vector<vector<Point2f> >& imagePoints, vector<Mat>& rvecs, vector<Mat>& tvecs);
+
+ //== Definitions of functions ==--
+
+int main(int argc, char* argv[])
+{
+    printf("==============================================================================\n");
+    printf("== Camera Calibration                       NaturalPoint 2010 ==\n");
+    printf("==============================================================================\n\n");
+	
+
+    //== This example attaches a frame synchronizer for all connected cameras.  Once
+    //== attached, it receives their synchronized frame data as a FrameGroup vs. the
+    //== GetFrame approach used for a single camera.
+
+    //== For OptiTrack Ethernet cameras, it's important to enable development mode if you
+    //== want to stop execution for an extended time while debugging without disconnecting
+    //== the Ethernet devices.  Lets do that now:
+
+    CameraLibrary_EnableDevelopment();
+
+	//== Next, lets wait until all connected cameras are initialized ==
+
+    printf("Waiting for cameras to spin up...");
+    
+    //== Initialize Camera Library ==----
+
+    CameraManager::X().WaitForInitialization();
+    
+    //== Verify all cameras are initialized ==--
+
+    if(CameraManager::X().AreCamerasInitialized())
+        printf("complete\n\n");
+    else
+    {
+        printf("failed (return key to exit)");
+        getchar();
+        return 1;
+    }
+
+    //== Connect to all connected cameras ==----
+
+    Camera *camera[kMaxCameras];
+    int     cameraCount=0;
+
+    CameraList list;
+
+    printf("Cameras:\n");
+
+    for(int i=0; i<list.Count(); i++)
+    {
+        printf("Camera %d >> %s\n", i, list[i].Name());
+
+        camera[i] = CameraManager::X().GetCamera(list[i].UID());
+        if(camera[i]==0)
+            printf("unable to connected to camera...\n");
+        else {
+            cameraCount++;
+			//camera[i]->SetVideoType(PrecisionMode); <-what's this? -NR
+			// values derived from visualtest.exe in camera SDK/bin -NR
+			camera[i]->SetExposure(50);
+			camera[i]->SetThreshold(200);
+			camera[i]->SetIntensity(15);
+		}
+    }
+        
+    if(cameraCount==0)
+    {
+        printf("no cameras (return key to exit)");
+        getchar();
+        return 1;
+    }
+
+    printf("\n");
+
+    //== Create and attach frame synchronizer ==--
+
+    cModuleSync * sync = new cModuleSync();
+    for(int i=0; i<cameraCount; i++)
+        sync->AddCamera(camera[i]);
+
+    //== Start cameras ==--
+
+    printf("Starting cameras... (any key to exit)\n\n");
+    for(int i=0; i<cameraCount; i++)
+        camera[i]->Start();
+
+
+	//== Define Image Size ==-- DBE
+	Size imageSize = Size(1280, 1024);
+
+	//== Get Image Points and Object Points==--
+	vector<vector<Point2f>> imagePoints = GetImagePoints(sync);
+	vector<vector<Point3f>> objectPoints = MakeObjectPoints();   //Only Works for our board (Alternating rows of 3 and 2 dots placed two inches apart horizontally and 1 inch vertically on a plane [z = 0])
+	
+	//Initialize Camera Matrix and distortion coefficient matrix
+	Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+	Mat distCoeffs = Mat::zeros(8, 1, CV_64F);
+
+	//Create rvecs and tvecs output vectors
+	vector<Mat> rvecs, tvecs;
+
+
+	//== Perform Calibration ==-- 
+	runCalibration(imageSize, cameraMatrix, distCoeffs, objectPoints, imagePoints, rvecs, tvecs);	
+
+	//== Print Calibration ==-- DBE
+	cout << "\nCalibration Results:\n" << endl << "distCoeffs = " << endl;
+	printMat(distCoeffs);
+	cout << "Camera Matrix" << endl;
+	printMat(cameraMatrix);
+
+    //== Destroy synchronizer ==--
+    sync->RemoveAllCameras();
+	delete sync;
+
+    //== Release cameras ==--
+    for(int i=0; i<cameraCount; i++)
+        camera[i]->Release();
+
+    //== Disconnect devices and shutdown Camera Library ==--
+    CameraManager::X().Shutdown();
+	
+	return 0;
+}
+
+
+
+
+vector<vector<Point2f>> GetImagePoints(cModuleSync * sync)
+{
+	//== Create Image Points Vector of Vectors ==-- DBE
+	vector<vector<Point2f>> imagePoints (RECORDED_FRAMES);
+	for(int i = 0; i < RECORDED_FRAMES; i++)
+		imagePoints[i].resize(NUMBER_OF_POINTS);
+	
+	//== Initialize Total Recorded Frame Count ==-- DBE
+	int TotalFrames = 0;
+
+	//== Pool for frame groups ==--
+    while(!_kbhit() && TotalFrames < RECORDED_FRAMES)
+    {
+        FrameGroup *frameGroup = sync->GetFrameGroup();
+
+        if(frameGroup)
+        {
+            //== Print something every 100 frames ==--
+
+            static int frameThrottle = X_FRAMES;
+            static int frameCount    = 0;
+            frameThrottle = (frameThrottle+1)%100;
+
+            frameCount++;
+
+            if(frameThrottle==0)
+            {
+                //== Ok, lets print something about this frame group ==--
+
+                printf("Received Group Frame #%d (Contains %d frames)   ", frameCount, frameGroup->Count());
+
+                if(sync->LastFrameGroupMode()==FrameGroup::Hardware)
+                    printf("Synchronized\n");
+                else
+                    printf("Unsynchronized\n");
+
+                for(int i=0; i<frameGroup->Count(); i++)
+                {
+                    Frame * frame = frameGroup->GetFrame(i);
+
+                    printf("  - Camera #%d is reporting %d 2D objects\n", i, frame->ObjectCount());
+					
+					// print the objects and display -NR
+					cv::Mat M;
+					M.create(frame->GetCamera()->Height(), frame->GetCamera()->Width(), CV_32F);
+					for(int j = 0; j < frame->ObjectCount(); j++) {
+						cObject *object = frame->Object(j);
+						printf("\tObject #%d: (%4.2f,%4.2f)\n", j, object->X(), object->Y());
+						cv::circle(M, cv::Point2f(object->X(), object->Y()), object->Radius(), cv::Scalar(255, 0, 0));
+						//== Record a point into imagePoints ==-- DBE
+						if(frame->ObjectCount() == NUMBER_OF_POINTS)
+						{
+							imagePoints[TotalFrames][j] = Point2f(object->X(), object->Y());
+						}
+					}
+
+					if(frame->ObjectCount() == NUMBER_OF_POINTS)
+						TotalFrames++;
+
+					if(frame->ObjectCount()) {
+						cv::imshow("objects", M);
+						cv::waitKey(2);
+					}
+
+                    frame->Release();
+                }
+
+                printf("\n");
+            }
+
+            frameGroup->Release();
+        }
+
+        Sleep(2);
+    }
+	return imagePoints;
+}
+
+vector<vector<Point3f>> MakeObjectPoints()
+{
+	vector<vector<Point3f>> objectPoints (RECORDED_FRAMES);
+	for (int i = 0; i < RECORDED_FRAMES; i++)
+		objectPoints[i].resize(NUMBER_OF_POINTS);
+	int x, y, z;
+	for( int i = 0; i < RECORDED_FRAMES; i++)
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+		for ( int j = 0; j < NUMBER_OF_POINTS; j++)
+		{
+			objectPoints[i][j] = Point3f(x, y, z);
+			if (y%2 == 0)
+			{
+				x= x +2;;
+				if (x > 4)
+				{
+					y++;
+					x = 1;
+				}
+			}
+			else
+			{
+				x = x + 2;
+				if (x > 4)
+				{
+					y++;
+					x = 0;
+				}
+			}
+		}
+	}
+	return objectPoints;
+}
 
 void printMat(const Mat& m)
 {
@@ -133,7 +393,7 @@ double mycalibrateCamera( InputArrayOfArrays _objectPoints,
     if( !(flags & CALIB_RATIONAL_MODEL) )
         distCoeffs = distCoeffs.rows == 1 ? distCoeffs.colRange(0, 5) : distCoeffs.rowRange(0, 5);
 
-    int    i;
+    int i;
     size_t nimages = _objectPoints.total();
     CV_Assert( nimages > 0 );
     Mat objPt, imgPt, npoints, rvecM((int)nimages, 3, CV_64FC1), tvecM((int)nimages, 3, CV_64FC1);
@@ -175,324 +435,30 @@ double mycalibrateCamera( InputArrayOfArrays _objectPoints,
     return reprojErr;
 }
 
-bool runCalibration( Size& imageSize, Mat& cameraMatrix, Mat& distCoeffs, vector<vector<Point3f> > objectPoints, 
-                    vector<vector<Point2f> > imagePoints, vector<Mat>& rvecs, vector<Mat>& tvecs)
+bool runCalibration( Size& imageSize, Mat& cameraMatrix, Mat& distCoeffs, vector<vector<Point3f> >& objectPoints, 
+                    vector<vector<Point2f> >& imagePoints, vector<Mat>& rvecs, vector<Mat>& tvecs)
 {
 	TermCriteria criteria=TermCriteria( TermCriteria::COUNT+TermCriteria::EPS, 30, DBL_EPSILON);
     //Find intrinsic and extrinsic camera parameters
-	double rms = mycalibrateCamera(objectPoints, imagePoints, imageSize, cameraMatrix,
+
+	vector<Mat> objpnts (RECORDED_FRAMES);
+	vector<Mat> imgpnts (RECORDED_FRAMES);
+	for(int i = 0; i < RECORDED_FRAMES; i++)
+	{
+		imgpnts[i] = Mat(imagePoints[i], true);
+		objpnts[i] = Mat(objectPoints[i], true);
+	}
+
+
+	double rms = mycalibrateCamera(objpnts, imgpnts, imageSize, cameraMatrix,
                                  distCoeffs, rvecs, tvecs, 0, criteria);
     
-	cout << "Re-projection error reported by calibrateCamera: "<< rms << endl;
+	imgpnts.clear();
+	objpnts.clear();
+
+	std::cout << "Re-projection error reported by calibrateCamera: "<< rms << endl;
 
     bool ok = checkRange(cameraMatrix) && checkRange(distCoeffs);
 
     return ok;
-}
-
-int main(int argc, char* argv[])
-{
-    printf("==============================================================================\n");
-    printf("== Frame Synchronization Example                          NaturalPoint 2010 ==\n");
-    printf("==============================================================================\n\n");
-	
-	Mat M(2,2, CV_8UC3, Scalar(0,0,255));
-	printMat(M);
-
-    //== This example attaches a frame synchronizer for all connected cameras.  Once
-    //== attached, it receives their synchronized frame data as a FrameGroup vs. the
-    //== GetFrame approach used for a single camera.
-
-    //== For OptiTrack Ethernet cameras, it's important to enable development mode if you
-    //== want to stop execution for an extended time while debugging without disconnecting
-    //== the Ethernet devices.  Lets do that now:
-
-    CameraLibrary_EnableDevelopment();
-
-	//== Next, lets wait until all connected cameras are initialized ==
-
-    printf("Waiting for cameras to spin up...");
-    
-    //== Initialize Camera Library ==----
-
-    CameraManager::X().WaitForInitialization();
-    
-    //== Verify all cameras are initialized ==--
-
-    if(CameraManager::X().AreCamerasInitialized())
-        printf("complete\n\n");
-    else
-    {
-        printf("failed (return key to exit)");
-        getchar();
-        return 1;
-    }
-
-    //== Connect to all connected cameras ==----
-
-    Camera *camera[kMaxCameras];
-    int     cameraCount=0;
-
-    CameraList list;
-
-    printf("Cameras:\n");
-
-    for(int i=0; i<list.Count(); i++)
-    {
-        printf("Camera %d >> %s\n", i, list[i].Name());
-
-        camera[i] = CameraManager::X().GetCamera(list[i].UID());
-        if(camera[i]==0)
-            printf("unable to connected to camera...\n");
-        else {
-            cameraCount++;
-			//camera[i]->SetVideoType(PrecisionMode); <-what's this? -NR
-			// values derived from visualtest.exe in camera SDK/bin -NR
-			camera[i]->SetExposure(50);
-			camera[i]->SetThreshold(200);
-			camera[i]->SetIntensity(15);
-		}
-    }
-        
-    if(cameraCount==0)
-    {
-        printf("no cameras (return key to exit)");
-        getchar();
-        return 1;
-    }
-
-    printf("\n");
-
-    //== Create and attach frame synchronizer ==--
-
-    cModuleSync * sync = new cModuleSync();
-
-    for(int i=0; i<cameraCount; i++)
-        sync->AddCamera(camera[i]);
-
-    //== Start cameras ==--
-
-    printf("Starting cameras... (any key to exit)\n\n");
-
-    for(int i=0; i<cameraCount; i++)
-        camera[i]->Start();
-
-    //== Create Image Points Vector of Vectors ==-- DBE
-	vector<vector<Point2f>> imagePoints (RECORDED_FRAMES);
-	for(int i = 0; i < RECORDED_FRAMES; i++)
-		imagePoints[i].resize(NUMBER_OF_POINTS);
-	
-	//== Initialize Total Recorded Frame Count ==-- DBE
-	int TotalFrames = 0;
-
-	//== Pool for frame groups ==--
-    while(!_kbhit() && TotalFrames < RECORDED_FRAMES)
-    {
-        FrameGroup *frameGroup = sync->GetFrameGroup();
-
-        if(frameGroup)
-        {
-            //== Print something every 100 frames ==--
-
-            static int frameThrottle = X_FRAMES;
-            static int frameCount    = 0;
-            frameThrottle = (frameThrottle+1)%100;
-
-            frameCount++;
-
-            if(frameThrottle==0)
-            {
-                //== Ok, lets print something about this frame group ==--
-
-                printf("Received Group Frame #%d (Contains %d frames)   ", frameCount, frameGroup->Count());
-
-                if(sync->LastFrameGroupMode()==FrameGroup::Hardware)
-                    printf("Synchronized\n");
-                else
-                    printf("Unsynchronized\n");
-
-                for(int i=0; i<frameGroup->Count(); i++)
-                {
-                    Frame * frame = frameGroup->GetFrame(i);
-
-                    printf("  - Camera #%d is reporting %d 2D objects\n", i, frame->ObjectCount());
-					
-					// print the objects and display -NR
-					cv::Mat M;
-					M.create(frame->GetCamera()->Height(), frame->GetCamera()->Width(), CV_32F);
-					for(int j = 0; j < frame->ObjectCount(); j++) {
-						cObject *object = frame->Object(j);
-						printf("\tObject #%d: (%4.2f,%4.2f)\n", j, object->X(), object->Y());
-						cv::circle(M, cv::Point2f(object->X(), object->Y()), object->Radius(), cv::Scalar(255, 0, 0));
-						//== Record a point into imagePoints ==-- DBE
-						if(frame->ObjectCount() == NUMBER_OF_POINTS)
-						{
-							imagePoints[TotalFrames][j] = Point2f(object->X(), object->Y());
-						}
-					}
-
-					if(frame->ObjectCount() == NUMBER_OF_POINTS)
-						TotalFrames++;
-
-					if(frame->ObjectCount()) {
-						cv::imshow("objects", M);
-						cv::waitKey(2);
-					}
-
-                    frame->Release();
-                }
-
-                printf("\n");
-            }
-
-            frameGroup->Release();
-        }
-
-        Sleep(2);
-    }
-    //== Create Object Points ==-- DBE
-	vector<vector<Point3f>> objectPoints (RECORDED_FRAMES);   //Point3f instead of 2f
-	for (int i = 0; i < RECORDED_FRAMES; i++)
-		objectPoints[i].resize(NUMBER_OF_POINTS);
-	int x, y, z;
-	for( int i = 0; i < RECORDED_FRAMES; i++)
-	{
-		x = 0;
-		y = 0;
-		z = 0;
-		for ( int j = 0; j < NUMBER_OF_POINTS; j++)
-		{
-			objectPoints[i][j] = Point3f(x, y, z);
-			if (y%2 == 0)
-			{
-				x= x +2;;
-				if (x > 4)
-				{
-					y++;
-					x = 1;
-				}
-			}
-			else
-			{
-				x = x + 2;
-				if (x > 4)
-				{
-					y++;
-					x = 0;
-				}
-			}
-		}
-	}
-///*
-	//== Print Object Points ==-- DBE
-	for( int i = 0; i < RECORDED_FRAMES; i++)
-	{
-		for ( int j = 0; j < NUMBER_OF_POINTS; j++)
-		{
-			std::cout << objectPoints[i][j];
-		}
-		cout << endl << endl;
-	}
-///*
-	//== Print Image Points ==-- DBE
-	for( int i = 0; i < RECORDED_FRAMES; i++)
-	{
-		for ( int j = 0; j < NUMBER_OF_POINTS; j++)
-		{
-			std::cout << imagePoints[i][j];
-		}
-		cout << endl << endl;
-	}
-//*/
-
-	//== Define Image Size ==-- DBE
-	Size imageSize = Size(1280, 1024);
-
-    //Initialize Camera Matrix
-	Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
-	cout << "cameaMatrix = ";
-	printMat(cameraMatrix);
-
-
-	// double aspectRatio=1;
-	 //Get initial Camera Matrix; doesn't work
-	// cameraMatrix=initCameraMatrix2D(objectPoints,imagePoints,imageSize,aspectRatio);
-
-
-	 
-	//Initialize distCoeffs
-	Mat distCoeffs;
-	distCoeffs = Mat::zeros(8, 1, CV_64F);
-
-	//Create rvecs and tvecs output vectors
-	vector<Mat> rvecs, tvecs;
-	
-
-	//== Define Flags ==-- DBE
-	int flags=0;
-
-	//== Define Criteria ==-- DBE
-	TermCriteria criteria=TermCriteria( TermCriteria::COUNT+TermCriteria::EPS, 30, DBL_EPSILON);
-
-	
-	cout<<"Frames in objectPoints: "<<objectPoints.size()<<endl;
-	cout<<"Frames in imagePoints:  "<<imagePoints.size()<<endl;
-	cout<<"Points in a frame of objectPoints: "<<objectPoints[2].size()<<endl;
-	cout<<"Points in a frame of imagePoints:  "<<imagePoints[2].size()<<endl;
-
-	cout<<"imageSize="<<imageSize.width<<","<<imageSize.height<<endl;
-	cout << "distCoeffs = ";
-	printMat(distCoeffs);
-
-	cout << "object points total: " << static_cast<InputArrayOfArrays>(objectPoints).total()  << endl;
-	
-
-	objectPoints.resize(imagePoints.size(),objectPoints[0]); 
-
-
-	runCalibration(imageSize, cameraMatrix, distCoeffs, objectPoints, imagePoints, rvecs, tvecs);
-
-	
-	//== Perform Calibration ==-- DBE
-	vector<Point3f> w0(1, Point3f(1,2,3));
-	vector<vector<Point3f>> w(1);
-	w[0] = w0;
-	vector<Point2f> p0(1, Point2f(17,5));
-	vector<vector<Point2f>> p(1);
-	p[0] = p0;
-	Mat A = Mat::zeros(3,3, CV_64F);
-	Mat k = Mat::zeros(8,1, CV_64F);
-	Size is = Size(10, 10);
-	vector<Mat> rv(1);
-	vector<Mat> tv(1);
-
-
-	//runCalibration(is, A, k, w, p, rv, tv);
-
-	//double Re_Projection_Error =  calibrateCamera(w, p, is, A, k, rv, tv, flags);
-
-//	double Re_Projection_Error =  calibrateCamera(w, p, is, A, k, rv, tv, flags);
-		//criteria);
-
-	
-
-	//== Print Calibration ==-- DBE
-	cout << "\nCalibration Results:\n" << endl;
-	cout << "Camera Matrix" << cameraMatrix << endl;
-	//cout << endl << "Re-projection Error: " << Re_Projection_Error << endl;
-
-    //== Destroy synchronizer ==--
-
-    sync->RemoveAllCameras();
-    delete sync;
-
-    //== Release cameras ==--
-
-    for(int i=0; i<cameraCount; i++)
-        camera[i]->Release();
-
-    //== Disconnect devices and shutdown Camera Library ==--
-
-    CameraManager::X().Shutdown();
-	return 0;
 }
