b3327cb3f28e06acf9bcc401f8f9c73a756375ca
James Sun <james.b.sun@gmail.com>
2013-06-04 10:15:09 -0500

    updating dspic pwm code

--- PCB/DsPIC PWM Test/main.c
+++ PCB/DsPIC PWM Test/main.c
@@ -9,48 +9,74 @@
 #include <stdlib.h>
 #include <p33Fxxxx.h>
 
+
 /*
  * 
  */
-int main(int argc, char** argv) {
 
-    // External clock = 8 MHz
 
-    // 8MHz/2*40/2 = 80 MHz = 40 MIPS
-    CLKDIVbits.PLLPRE=2;        // PLLPRE divide input by 2
-    PLLFBD = 40;                // pll multiplier
+/* Configuration Bit Settings */
+_FOSCSEL(FNOSC_FRC)
+_FOSC(FCKSM_CSECMD & OSCIOFNC_ON)
+_FWDT(FWDTEN_OFF)
+_FPOR(FPWRT_PWR128)
+_FICD(ICS_PGD2 & JTAGEN_OFF)
+
+int main() {
+
+    //Configure oscillator freq Fosc
+    // Fin = External oscillater clock = 8 MHz
+    // Fosc = Fin*M/(N1*N2)
+    // 8MHz*40/(2*2) = 80 MHz
+    // Fcy = Fosc/2 = 40 MIPS
+
+    //TRISBbits.TRISB15 = 0;
+    //LATBbits.LATB15 = 1;
+    CLKDIVbits.PLLPRE=2;        // N1 = PLLPRE-2
+    PLLFBD = 40;                // M = PLLFBD-2
     CLKDIVbits.PLLPOST=0;       // PLLPOST (N1) 0= DIV_2
 
     CLKDIVbits.PLLPRE -= 2;
     PLLFBD -= 2;
 
+    __builtin_write_OSCCONH(0x01);	/* New Oscillator FRC w/ PLL */
+    __builtin_write_OSCCONL(0x01);  	/* Enable clock switch Switch */
+    
     RCONbits.SWDTEN = 0; // Disable Watch Dog Timer
+    
+    while(OSCCONbits.COSC != 0b001); //Wait for new Oscillator to become FRC w/ PLL */
     while(!OSCCONbits.LOCK);    // wait for PLL ready
 
-    //init pwm
-    PTCON2bits.PCLKDIV = 0b000; //clock divider = 1
+    ACLKCONbits.FRCSEL = 1;	/* FRC provides input for Auxiliary PLL (x16) */
+    ACLKCONbits.SELACLK = 1;	/* Auxiliary Oscillator provides clock source for PWM & ADC */
+    ACLKCONbits.APSTSCLR = 7;	/* Divide Auxiliary clock by 1 */
+    ACLKCONbits.ENAPLL = 1;	/* Enable Auxiliary PLL */
 
-    //pll period = 1.25 ns;
-    //PWM period  = 1.25ns/2*PTPER
-    PTPER = 80000; // PWM period = 50 us
-    MDC = 40000; //50% duty cycle
-    //PDC1 = 40000;
-    //SDC1 = 40000; //50% duty cycle for PWM1L in independent mode
-    PHASE1 = 0; //No phase shift
-    //PHASE2 = 0;
+    while(ACLKCONbits.APLLCK != 1);			/* Wait for Auxiliary PLL to Lock */
+
+
+
+    //init pwm
+    //PWM period  = PTPER*1.04ns*PCLKDIV
+    PTCON2bits.PCLKDIV = 0b101; //PCLKDIV = 32
+    PTPER = 3004; // PWM period = 100 us => freq = 10kHz
+        
     PWMCON1bits.FLTIEN = 0; // disable fault int
     PWMCON1bits.CLIEN = 0; //Current limit int disabled
     PWMCON1bits.TRGIEN = 0; //trigger in disabled
     PWMCON1bits.ITB = 0; //use PTPER for period;
-    PWMCON1bits.MDCS = 1; //use MDC for duty cycle, can set to 0 and then set using PDCx
-    PWMCON1bits.DTC = 0b10;// disable dead time
+    PWMCON1bits.MDCS = 0; //use PDC1 for duty cycle
+    PWMCON1bits.DTC = 0b00;// enable dead time
     IOCON1bits.PENH = 1; //PWM controls PWM1H
     IOCON1bits.PENL = 1; //PWM controls PWM1L
     IOCON1bits.POLH = 0; //PWM1H active high (default)
     IOCON1bits.POLL = 0; //PWM1L active low O(default)
-    IOCON1bits.PMOD = 0b00; //Complementary output mode
+    IOCON1bits.PMOD = 0; //Complementary output mode
+    PDC1 = 1502; //50% duty cycle
+    DTR1 = 30; //1% dead time
+    ALTDTR1 = 30;
+    PHASE1 = 0; //No phase shift
     PTCONbits.PTEN = 1; //enable pwm
-
-    return (EXIT_SUCCESS);
+    while(1);
 }
 
