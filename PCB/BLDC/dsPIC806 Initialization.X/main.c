85b44194125e462e301d12f0f182546eb9e720c1
Andrew Griesemer <ajgriesemer@gmail.com>
2013-09-03 13:57:14 -0500

    Created dsPIC806 Code Porting. This project now runs the dsPIC33EP512MC806. I got change notification up and running for the hall effecto sensor pins. I confirmed that digital I/O works on the USER, LED and motor control pins.

--- PCB/BLDC/dsPIC806 Initialization.X/main.c
+++ PCB/BLDC/dsPIC806 Initialization.X/main.c
@@ -1,18 +1,29 @@
-/* 
- * File:   main.c
- * Author: ajgriesemer
- *
- * Created on September 3, 2013, 9:41 AM
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <p33EP512MC806.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
 
-    return (EXIT_SUCCESS);
+/* Configuration Bit Settings */
+_FOSCSEL(FNOSC_FRCPLL & IESO_OFF)
+_FOSC(OSCIOFNC_OFF)
+//_FWDT(FWDTEN_OFF)
+//_FPOR(FPWRT_PWR1 & BOREN_OFF & ALTI2C1_OFF)
+//_FICD(ICS_PGD1 & JTAGEN_OFF)
+//Communicate on PGC1/EMUC1 and PGD1/EMUD1
+//JTAG is Disabled
+
+int main(void) {
+
+    //Fsys = Fin * M/ (N1 * N2)
+    //Fsys = 7.37 MHz * 44/ (2*2) = 80MHz
+    CLKDIVbits.PLLPRE = 0;   //N1 = 2
+    PLLFBDbits.PLLDIV = 42; //M = 44
+    CLKDIVbits.PLLPOST = 0;  //N2 = 2
+
+    TRISBbits.TRISB15 = 0;
+
+    while (1){
+        LATBbits.LATB15 = !LATBbits.LATB15;
+    }
+    return 0;
 }
-
