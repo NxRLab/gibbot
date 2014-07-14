fd57d2d2e4304e042f2926d88ebff773486412ee
Katie <katherinelatimer2013@u.northwestern.edu>
2014-07-14 17:08:22 -0500

    added drag and drop in BananaPanel1

--- Code/Java_Projects/gui/guisrc/BananaPanel1.java
+++ Code/Java_Projects/gui/guisrc/BananaPanel1.java
@@ -13,11 +13,12 @@ import java.awt.geom.*;
 
 /*Handles user placement of bananas, animation of robot, and draws metal board background*/
 
-public class BananaPanel1 extends JPanel implements MouseListener, ActionListener {
+public class BananaPanel1 extends JPanel implements MouseListener, MouseMotionListener, ActionListener {
 	
 	Banana banana;
 	Gibbot bob = new Gibbot();
 	boolean interacting = false;
+	boolean dragging = false;
 	Font f = new Font("Serif", Font.BOLD, 30);
 	Timer timer = new Timer(40, this);
 	double slope;
@@ -33,6 +34,7 @@ public class BananaPanel1 extends JPanel implements MouseListener, ActionListene
     	setPreferredSize(new Dimension(width, height));
     	this.setBackground(Color.WHITE);
     	this.addMouseListener(this);
+    	this.addMouseMotionListener(this);
     }
     
     public void paintComponent(Graphics g) {
@@ -44,42 +46,89 @@ public class BananaPanel1 extends JPanel implements MouseListener, ActionListene
 		g.drawRect(40, 40, getWidth()-80, getHeight()-80);
     	g.drawRect(50, 50, getWidth()-100, getHeight()-100);    	
     	Graphics2D g2d = (Graphics2D)g;
+    	g2d.setFont(f);
     	g2d.drawImage(board.getImage(), 60, 60, getWidth()-120, getHeight()-120, this);
+    	
+    	g2d.setColor(Color.RED);
+    	g2d.fillRect((int)(.91*getWidth())-105, (int)(.75*getHeight())-15, (int)(.09*getWidth()), (int)(.25*getHeight()));
+    	g2d.setColor(Color.BLACK);
+    	g2d.drawString("Bananas", (int)(.91*getWidth())-95, (int)(.75*getHeight())+20);
     	
     	g2d.setColor(Color.BLACK);
 
-    	if(!interacting){												//AKA if the user hasn't clicked on the board yet
-    		g2d.setFont(f);
-    		g2d.drawString("Click to place the bait!", 0, 25);
+    	if(!interacting){												//Either nothing has happened or the user is moving the banana
+    		if(dragging)
+    			g2d.drawImage(banana.getImage(), banana.getX(), banana.getY(), 60, 60, this);												//AKA if the user hasn't clicked on the board yet
+    		
+    		g2d.drawString("Click and drag to place the bait!", 0, 25);
     		bob.draw(g);
-    		//swipe tab draw methods
     	}
-    	else{															//If the user has clicked, this is where we draw call for animation images
-        g2d.drawImage(banana.getImage(), banana.getX(), banana.getY(), 60, 60, this);
-        bob.draw(g);
-        //swipe tab draw methods        
+    	
+    	else{															//If the user dropped the banana, this is where we draw call for animation images
+        	g2d.drawImage(banana.getImage(), banana.getX(), banana.getY(), 60, 60, this);
+        	bob.draw(g);    
     	}        
     	
     }
     
-    public void mouseClicked(MouseEvent evt) {
+    public void mousePressed(MouseEvent evt) {
     	
-    	if(!interacting){					//If the panel has been waiting to be clicked on and nothing is happening, this starts the animation in motion.
-    										//NOTE: also need some restriction on target coors to keep real robot from falling off board
-    		interacting = true;
-    		slope = (double)(evt.getY()-bob.getPivotY())/(double)(evt.getX()-bob.getPivotX());    	
-    		banana = new Banana();
-    		
-    		banana.setX(evt.getX());
-    		banana.setY(evt.getY());
-    		
-    		//SERIAL: send goal coordinates to robot
-    	
-    		repaint();
-    		timer.start();
-    	}
-    	else								//If the user clicks while the animation is going on, nothing happens.
+    	if(interacting)						//If the user clicks while the animation is going on, nothing happens.
     		return;
+    	
+    	int x = evt.getX();
+    	int y = evt.getY();
+    	
+    	if((int)((.91*getWidth())-105)<x && x<(getWidth()-105) && (.75*getHeight()-15)<y && y<(getHeight()-15)){
+    	
+    		if(!dragging){					//If the panel has been waiting to be clicked on and nothing is happening, this draws a banana to be moved around.
+    										
+    			dragging = true;   	
+				banana = new Banana();
+    		
+    			banana.setX(x);
+    			banana.setY(y);
+    	
+    			repaint();
+    		
+    	}
+   	}
+   	
+    	else								
+    		return;
+  }
+   
+   public void mouseDragged(MouseEvent evt){
+   	
+   	int x = evt.getX();
+    int y = evt.getY();
+   	
+   	if(dragging){
+   		banana.setX(x);
+    	banana.setY(y);
+    	
+    	repaint();
+   		
+   	}
+   	
+   	
+   }
+   
+   public void mouseExited(MouseEvent evt){
+   	
+   	if(dragging){
+   		dragging = false;
+   		repaint();
+   	}
+   	}
+   
+   public void mouseReleased(MouseEvent evt){
+   	if(dragging){								//NOTE: also need some restriction on target coors to keep real robot from falling off board
+   		dragging = false;
+   		interacting = true;
+   		timer.start();
+   	}
+   		
    }
    
    public void actionPerformed(ActionEvent evt){//Handles events from the timer
@@ -104,9 +153,8 @@ public class BananaPanel1 extends JPanel implements MouseListener, ActionListene
    		
    }
    /*Required methods for MouseListener interface*/
-   public void mousePressed(MouseEvent evt){}
-   public void mouseReleased(MouseEvent evt){}
+   public void mouseClicked(MouseEvent evt){}
    public void mouseEntered(MouseEvent evt){}
-   public void mouseExited(MouseEvent evt){}
+   public void mouseMoved(MouseEvent evt){}
     
 }
