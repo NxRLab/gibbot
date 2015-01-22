b84cc1abc93d3390096ff5b2bf08b8301744bd9b
nxrlab <nxrlab@gmail.com>
2015-01-22 12:11:52 -0600

    using as test file for learning about conflicts.

--- Code/Java_Projects/gui3/rungui3.sh
+++ Code/Java_Projects/gui3/rungui3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #git pull
 cp ../imagefiles/* .
-javac -d ./ -cp "../newserial/jssc-2.8.0.jar;gui3src" gui3src/GibbotGUI3.java
-java -cp ".;../newserial/jssc-2.8.0.jar;" GibbotGUI3 $1
+#javac -d ./ -cp "../newserial/jssc-2.8.0.jar;gui3src" gui3src/GibbotGUI3.java
+javac -d ./ -cp "../newserial/jssc-2.8.0.jar:gui3src" gui3src/*.java
+java -cp ".:../newserial/jssc-2.8.0.jar:" GibbotGUI3 $1
