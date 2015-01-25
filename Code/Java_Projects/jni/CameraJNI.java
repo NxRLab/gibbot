abe8f9e6bd450f34060f728176ded92d8f7a27ef
brigittebro <brigitte.broszus@gmail.com>
2015-01-24 20:50:56 -0600

    Adding files
    Added compiled versions of arrays.cpp (arrays.exe)
    Uncommented arrays.cpp
    Duplicated Arrays and named CameraJNI.java
    Duplicated arrays and named camerajni.cpp
    Added header file of CameraJNI.h

---
+++ Code/Java_Projects/jni/CameraJNI.java
@@ -0,0 +1,20 @@
+public class CameraJNI{
+    // make proper modifications to this method to return myarr in arrays.cpp
+    public static double[] GetArrayFromC()
+    {
+        double[] r = {100, 200, 300, 400, 500, 600, 700};
+        return r;
+    }
+
+
+    // use main to test your JNI code
+    public static void main(String args[]) 
+    {
+        double[] arr = Arrays.GetArrayFromC();
+
+        System.out.println("The values of arr are:");
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println("arr[i] = " + arr[i]);
+        }
+    }
+}
