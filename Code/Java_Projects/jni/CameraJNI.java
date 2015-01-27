1643b8d97ca97d5ade15a5f9ebcf49b0030755aa
brigittebro <brigitte.broszus@gmail.com>
2015-01-27 04:12:25 -0600

    JNI modifications to CameraJNI
    Added 'native' notation in method declaration
    Added System.loadLibrary, named "camera" which will be used to create
    DLL
    Renamed reference of old class Array to CameraJNI

--- Code/Java_Projects/jni/CameraJNI.java
+++ Code/Java_Projects/jni/CameraJNI.java
@@ -1,18 +1,21 @@
 public class CameraJNI{
     // make proper modifications to this method to return myarr in arrays.cpp
-    public static double[] GetArrayFromC()
+    public static native double[] GetArrayFromC();
+    static
     {
-        double[] r = {100, 200, 300, 400, 500, 600, 700};
-        return r;
+	System.loadLibrary("camera");
+        //double[] r = {100, 200, 300, 400, 500, 600, 700};
+        //return r;
     }
 
 
     // use main to test your JNI code
     public static void main(String args[]) 
     {
-        double[] arr = Arrays.GetArrayFromC();
+        double[] arr = CameraJNI.GetArrayFromC();
 
-        System.out.println("The values of arr are:");
+        
+	System.out.println("The values of arr are:");
         for(int i = 0; i < arr.length; i++) {
             System.out.println("arr[i] = " + arr[i]);
         }
