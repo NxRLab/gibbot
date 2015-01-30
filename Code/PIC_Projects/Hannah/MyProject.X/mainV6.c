e4b9b37adb39f8edfe156951b199796aea7ceeeb
Hannah Arntson <harntson@localhost.localdomain>
2015-01-30 12:32:06 -0600

    Hannah PIC Projects

---
+++ Code/PIC_Projects/Hannah/MyProject.X/mainV6.c
@@ -0,0 +1,27 @@
+/* First iteration of code to go with the Gibbot v5 control board. This code
+ * will be developed to interface with a python controller on the PC that
+ * can drive the motor, control the moagnets and read data from the Gibbot.
+ */
+
+#include <p33EP512MC806.h>
+#include "initializeV6.h"
+#include "motor.h"
+#include "test.h"
+
+int main(void) {
+    initialize();
+    initialize_ADC_Offset();
+    while (1){
+        if(USER){
+            //LED1 = !LED1;
+            //LED2 = !LED2;
+            //LED3 = !LED3;
+            //LED4 = !LED4;
+            TOPMAG=1;
+            //TOPMAG = !TOPMAG;
+            //commutate(7);
+        }
+       test_MayDay();
+    }
+    return 0;
+}
