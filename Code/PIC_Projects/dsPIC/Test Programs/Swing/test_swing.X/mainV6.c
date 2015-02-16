cf08a59bdd304356b48419c3b3b8bbe521ff5351
Hannah Arntson <hannaharntson@gmail.com>
2015-02-16 10:49:24 -0600

    Created program to test swinging

---
+++ Code/PIC_Projects/dsPIC/Test Programs/Swing/test_swing.X/mainV6.c
@@ -0,0 +1,79 @@
+/* First iteration of code to go with the Gibbot v5 control board. This code
+ * will be developed to interface with a python controller on the PC that
+ * can drive the motor, control the moagnets and read data from the Gibbot.
+ */
+
+#include <p33EP512MC806.h>
+#include <stdio.h>
+#include "initializeV6.h"
+#include "motor.h"
+#include "test.h"
+#include "time.h"
+#include "UART.h"
+#include "encoder.h"
+
+int main(void) {
+    //initialize all peripherals, encoder values
+    initialize();
+    initialize_ADC_Offset();
+    initialize_QEI();
+    initialize_UART();
+
+    unsigned char c='o';
+    motoron = 1;
+    direction = CW;
+    //write_duty(300);
+    //timer1_on();
+
+    while (1) {
+        c = read_UART();
+
+        //commands to control specific components
+        if (c=='o'){
+            TOPMAG=1;
+        }
+        else if (c=='f'){
+            TOPMAG=0;
+        }
+        else if(c == 'g') {
+            //Turn on Bottom Magnet
+            write_UART2('1');
+        } else if(c =='h') {
+             //Turn off Bottom Magnet
+            write_UART2('2');
+        }
+        else if (c=='u'){
+            // inc duty
+            // for now, increase the duty by 10% of max duty
+            write_duty(read_duty()+100);
+            c = 'x';
+        }
+        else if (c=='d'){
+            // dec duty
+            write_duty(read_duty()+100);
+            c = 'x';
+        }
+        else if (c=='m'){
+            // motor off
+            motoron = 0;
+        }
+        else if (c=='x'){
+            // make sure duty is only updated once per call
+            continue;
+        }
+
+        //command to swing robot
+        else if (c=='l'){
+            //swing left
+            TOPMAG=0;
+            write_duty(200);
+            
+            //delay function in progress
+            delay();
+
+            write_duty(0);
+            TOPMAG=1;
+        }
+    }
+    return 0;
+}
---
+++ Code/PIC_Projects/dsPIC/Test Programs/Swing/test_swing.X/time.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   time.h
+ * Author: Hannah Arntson
+ *
+ * Created on February 1, 2015
+ */
+
+ #ifndef TIME_H
+ #define TIME_H
+
+ void timer1_on(void);
+ void timer1_off(void);
+ void delay(void);
+
+ #endif /*TIME_H*/
