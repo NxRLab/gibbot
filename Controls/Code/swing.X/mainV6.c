0e9ffbdbb7f0e776663d56e38c19f6bcbadea2fa
Hannah Arntson <hannaharntson@gmail.com>
2015-03-06 13:35:01 -0600

    Input modifications to swing.X

--- Controls/Code/swing.X/mainV6.c
+++ Controls/Code/swing.X/mainV6.c
@@ -9,7 +9,8 @@
 
 int main(void) {
     initialize();
-    
+
+    unsigned char buf[10];
     unsigned char c='0';
     unsigned char state='x';
     int n=0;
@@ -18,18 +19,22 @@ int main(void) {
     struct args* control_args = (struct args*)malloc(N_MAX * sizeof(struct args));
 
     //Give control arguments to PIC, check parameters and run swing
-        printf("Press w to write, r to read, and x to swing\n");
+    printf("Press w to write, r to read, and x to swing\n");
+    printf("Input the number of arguments\n");
+    printf("If reading, input the state you want to check\n"); //or 'n' for all
 
     while (1) {
-        c = read_UART();
+        read_string_UART(buf,10);
+        sscanf(buf,"%c %d %c",c,&n,state);
+        
         if(c=='w'){
-            printf("Input n: \n");
-            scanf("%d",&n);
+            //printf("Input n: \n");
+            //scanf("%d",&n);
             write_swing(n, control_args);
         }
         else if(c=='r'){
-            printf('Enter n to print everything, or specify state to check\n');
-            state = read_UART();
+            //printf('Enter n to print everything, or specify state to check\n');
+            //state = read_UART();
             read_swing(state,n,control_args);
         }
         else if(c=='x'){
