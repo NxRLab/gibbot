9c912c25e9c3871a430bb255a0f6d0447941c033
nxrlab <nxrlab@gmail.com>
2015-04-04 01:03:47 -0500

    removing some old code that was never used (kept as reference) and removed DMA code.

--- Code/Embedded/include/pic2pic.h
+++ Code/Embedded/include/pic2pic.h
@@ -3,41 +3,9 @@
 
 /// @file
 
-extern void init_pic2pic(void);
 extern bool is_pic2pic_on(void);
+extern void init_pic2pic(void);
 extern int write_pic2pic(void *buffer, unsigned int len);
 extern int read_pic2pic(void *buffer, unsigned int len);
-extern void enable_dma5_on_tx();
-extern void enable_dma4_on_rx();
-
-struct OtherPICsData {
-    long int magnet_enc;
-    long int motor_enc;
-};
-
-typedef struct OtherPICsData OtherPICsData;
-
-extern volatile OtherPICsData other_boards_data;
-
-
-
-// bit fields are least to most significant bit p. 141 XC16 C Compiler User's
-// Guide
-struct packet {
-    union {
-        struct { 
-            unsigned char handle : 7;
-            unsigned char is_read : 1;
-            unsigned char data;
-        };
-
-        struct {
-            unsigned char cmd;
-            unsigned char : 0;
-        };
-    };
-};
-
-typedef struct packet packet;
 
 #endif /* PIC2PIC_H */
--- Code/Embedded/src/pic2pic.c
+++ Code/Embedded/src/pic2pic.c
@@ -5,19 +5,6 @@
 /// The PIC-to-PIC baud rate
 //#define PIC_BAUD (16UL*115200UL)
 #define PIC_BAUD (115200UL)
-#define RX_PRIORITY 5 // low = 1, high = 7
-#define TX_PRIORITY 5 // low = 1, high = 7
-#define DMA_RX_IRQ 30
-#define DMA_TX_IRQ 31
-#define INTERRUPT_EVERY_CHAR 0
-#define DMA_BYTE_TRFRS 1
-#define DMA_ONE_SHOT 1
-#define DMA_WRITE_TO_PERIPH 1
-#define DMA4_RX_PRIORITY 5 // low = 1, high = 7
-#define DMA5_TX_PRIORITY 5 // low = 1, high = 7
-
-volatile __eds__ GIBDMA OtherPICsData rx_data, tx_data;
-volatile OtherPICsData other_boards_data;
 
 /// Returns true if the PIC-to-PIC peripheral is present and enabled
 bool is_pic2pic_on()
@@ -100,88 +87,6 @@ int read_pic2pic(void *buffer, unsigned int len)
     // return number of bytes read
     return len - i;
 }
-
-void enable_dma4_on_rx()
-{
-    // UART interrupt settings
-    U2MODEbits.UARTEN = false;
-    U2STAbits.URXISEL = INTERRUPT_EVERY_CHAR;
-
-    // set up DMA for RX
-    DMA4CONbits.SIZE = DMA_BYTE_TRFRS;
-    DMA4CONbits.MODE = DMA_ONE_SHOT;
-    DMA4REQbits.IRQSEL = DMA_RX_IRQ; 
-    DMA4PAD = (VUI) &U2RXREG;
-    DMA4STAL = __builtin_dmaoffset(&rx_data);
-    DMA4CNT = sizeof(rx_data) - 1;
-
-    _DMA4IP = DMA4_RX_PRIORITY;
-    _DMA4IF = false;
-    _DMA4IE = true;
-
-    // enable the peripherals
-    DMA4CONbits.CHEN = true;
-    U2MODEbits.UARTEN = true;
-    U2STAbits.UTXEN = true;
-}
-
-// took 0.975 us --- given baud rate 2 32-bit numbers would take 34.72 us
-void GIBINT _DMA4Interrupt(void)
-{
-    _DMA4IF = false;
-
-    // transfer data to buffered output
-    other_boards_data = rx_data;
-
-    // start DMA4 again
-    DMA4STAL = __builtin_dmaoffset(&rx_data);
-    DMA4CONbits.CHEN = true;
-}
-
-void enable_dma5_on_tx()
-{
-    // set up initial transfer
-    tx_data.magnet_enc = my_magnet_enc.read();
-    tx_data.motor_enc = my_motor_enc.read();
-
-    // set TX to interrupt after every byte
-    U2MODEbits.UARTEN = false;
-    U2STAbits.UTXISEL0 = INTERRUPT_EVERY_CHAR;
-    U2STAbits.UTXISEL1 = INTERRUPT_EVERY_CHAR;
-
-    // set up DMA for TX
-    DMA5CONbits.SIZE = DMA_BYTE_TRFRS;
-    DMA5CONbits.MODE = DMA_ONE_SHOT;
-    DMA5CONbits.DIR = DMA_WRITE_TO_PERIPH;
-    DMA5REQbits.IRQSEL = DMA_TX_IRQ; 
-    DMA5PAD = (VUI) &U2TXREG;
-    DMA5STAL = __builtin_dmaoffset(&tx_data);
-    DMA5CNT = sizeof(tx_data) - 1;
-
-    // set up DMA5 interrupt
-    _DMA5IP = DMA5_TX_PRIORITY;
-    _DMA5IF = false;
-    _DMA5IE = true;
-
-    // enable the peripheral
-    DMA5CONbits.CHEN = true;
-    U2MODEbits.UARTEN = true;
-    U2STAbits.UTXEN = true;
-}
-
-// interrupt took  2.35 us to run
-void GIBINT _DMA5Interrupt(void)
-{
-    _DMA5IF = false;
-
-    tx_data.magnet_enc = my_magnet_enc.read();
-    tx_data.motor_enc = my_motor_enc.read();
-
-    DMA5STAL = __builtin_dmaoffset(&tx_data);
-    DMA5CONbits.CHEN = true;
-    DMA5REQbits.FORCE = true;
-}
-
 
 // TODO make sure that all periphs that are not enabled do not get used since
 // they're associated data types and functions may be null or undefined.
